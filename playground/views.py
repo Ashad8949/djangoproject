2b4a6a1396701aa2bb165e7880d3717c32330da3
Ashad Alam <ashad8949@gmail.com>
2023-01-19 23:53:18 +0530

    added

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
+from store.models import Product, OrderItem
 
 # Create your views here.
 # request -> response 
 # request handler
 # actions (other framework)
 
-def calculate():
-    x = 1 
-    y = 2 
-    return x
-
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name' : 'Ashad'})
-
-
+    queryset = Product.objects.filter(id__in=
+    OrderItem.objects.values('product_id').distinct()).order_by('title')
+    return render(request, 'hello.html', {'name' : 'Ashad', 'products': list(queryset)})
